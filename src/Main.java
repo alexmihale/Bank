190baf58d35c30f051d00665fa50c3cfd38019b8
alexmihale <andrei.mihale97@gmail.com>
2019-05-25 22:18:56 +0300

    le

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import bank.Currency;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount Radu = new BankAccount("Radu George", Currency.USD);
-        BankAccount George = new BankAccount("George Mihai", Currency.RON);
-        BankAccount Mihai = new BankAccount("Mihai Radu", Currency.EUR);
-        BankAccount Jin = new BankAccount("Jin Yoh", Currency.YEN);
+        BankAccount Radu = new BankAccount("Radu George", Currency.USD, "5468050458400842");
+        BankAccount George = new BankAccount("George Mihai", Currency.RON,"5468555458400842");
+        BankAccount Mihai = new BankAccount("Mihai Radu", Currency.EUR,"5468452458400842");
+        BankAccount Jin = new BankAccount("Jin Yoh", Currency.YEN,"546851448400842");
 
         Bank.ING.registerBankAccount(Radu);
         Bank.ING.registerBankAccount(George);
@@ -37,5 +37,7 @@ public class Main {
         System.out.println(Mihai.toString());
         System.out.println(Jin.toString());
 
+        System.out.println(Radu.verifyCreditCard("5468050458400842"));
+        System.out.println(Mihai.verifyCreditCard("5468452458400842"));
     }
 }
